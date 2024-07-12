a6275e7c987058ab5769dd3d4cf319df6789aec1
Aatif Syed <aatifsyedyp@gmail.com>
2024-07-12 07:38:43 +0100

    doc: main

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,38 @@
 //! - Implements [`Copy`] (and can only hold [`Copy`] types).
 //! - Does not grow.
 //! - Is `#[no_std]`/no-`alloc` compatible.
+//!
+//! ```
+//! # use copyvec::{CopyVec, copyvec};
+//!
+//! // const-friendly
+//! const VEC: CopyVec<&str, 10> = CopyVec::new();
+//!
+//! // easy initialising
+//! let mut vec = copyvec!["a", "b", "c"; + 2];
+//!                                    // ^~ with excess capacity
+//!
+//! // use the API's you know
+//! vec.push("d");
+//!
+//! // including iteration
+//! for it in &mut vec {
+//!     if *it == "a" {
+//!         *it = "A"
+//!     }
+//! }
+//!
+//! assert_eq!(vec, ["A", "b", "c", "d"]);
+//! vec.retain(|it| *it == "b" || *it == "c")
+//! assert_eq!(vec.remove(0), "b");
+//! ```
+//!
+//! # Other features
+//! - [`serde`](https://docs.rs/serde/)
+//! - [`quickcheck`](https://docs.rs/quickcheck/)
+//!
+//!
+//! If you like this crate, you may also enjoy [`stackstack`](https://docs.rs/stackstack)
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
